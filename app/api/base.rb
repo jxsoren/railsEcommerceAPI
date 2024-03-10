014d0632e3a1ad9c2ae7519bad36fcd614afea1e
jxsoren <jxsoren@gmail.com>
2024-03-09 20:30:41 -0700

    corrected name for module (namespace)

--- app/api/base.rb
+++ app/api/base.rb
@@ -1,5 +1,12 @@
-module EcommerceAPI
+module EcommerceApi
   class Base < Grape::API
+    version 'v1', using: :path
+    format json
+    prefix :api
+
+    get :ping do
+      {message: 'pong'}
+    end
 
   end
 end
