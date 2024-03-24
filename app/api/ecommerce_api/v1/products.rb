5a91978d9fae23e8115c94ad1fc3687d166d5a78
jxsoren <jxsoren@gmail.com>
2024-03-24 17:53:12 -0600

    corrected entity + removed unused message

--- app/api/ecommerce_api/v1/products.rb
+++ app/api/ecommerce_api/v1/products.rb
@@ -1,17 +1,14 @@
 module EcommerceApi
   module V1
-
     class Products < Grape::API
       resource :products do
         # GET /products
+        desc 'Products'
         get do
-
           # Fetch all products
           products = Product.all
-
           # Return products
-          present products, with: Entity::ProductEntity
-          { message: "You're getting products" }
+          present products, with: EcommerceApi::V1::Entities::ProductEntity
         end
 
         # POST /products
