5a83ea70000810740793f4696a61d00bcf754f92
jxsoren <jxsoren@gmail.com>
2024-03-24 17:52:47 -0600

    Product Entity init

--- app/api/ecommerce_api/v1/entities/product_entity.rb
+++ app/api/ecommerce_api/v1/entities/product_entity.rb
@@ -2,9 +2,14 @@ module EcommerceApi
   module V1
     module Entities
       class ProductEntity < Grape::Entity
-        expose :id
-        expose :name
-        expose :price
+        expose :id, documentation: {type: 'Integer', desc: 'Product ID'}
+        expose :name, documentation: {type: 'String', desc: 'Product Name'}
+        expose :price, documentation: {type: 'Integer', desc: 'Product Price'}
+        expose :stock_quantity, documentation: {type: 'Integer', desc: 'Product Stock Quantity'}
+        expose :sku, documentation: {type: 'String', desc: 'Product SKU'}
+        expose :is_active, documentation: {type: 'Bool', desc: 'Product Status'}
+        expose :weight, documentation: {type: 'Float', desc: 'Product Weight'}
+        expose :rating, documentation: {type: 'String', desc: 'Product Rating'}
       end
     end
   end
